ee5d56d5f47c6685ff12e9af163e233dae57adea
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-02 16:58:08 +0400

    A5

---
+++ src/A5_Person.java
@@ -0,0 +1,9 @@
+public class A5_Person {
+    public String name;
+    public int age;
+
+    public A5_Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/A5_Student.java
@@ -0,0 +1,11 @@
+public class A5_Student extends A5_Person {
+    public int grade;
+
+    public A5_Student(String name, int age, int grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+
+    public void giveGrade(){}
+    public void praise(){}
+}
