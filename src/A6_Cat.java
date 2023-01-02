4b31d1fd8f3b38579b82eacf4141cbdfa1d44b09
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-02 17:06:04 +0400

    A7

--- src/A6_Cat.java
+++ src/A6_Cat.java
@@ -1,16 +1,42 @@
 public class A6_Cat {
-    String name;
-    int age;
-    String owner;
+    private String name;
+    private int age;
+    private String owner;
 
     public A6_Cat(String name, int age, String owner) {
         this.name = name;
         this.age = age;
         this.owner = owner;
     }
+    void eat(){}
+    void run(){}
 
     @Override
     public String toString() {
         return "The cat " + name + " is already " + age + ". Her master " + owner + " takes care of her.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 }
