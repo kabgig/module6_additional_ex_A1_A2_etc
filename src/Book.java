421f2c3087479fb87bab057cb88d41d655089d7a
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-04 21:19:03 +0400

    A14 process2

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,26 @@
 public class Book extends PrintEdition{
+
+    public Book(String name, int quantityOfPages, int year, String author, String[] pages) {
+        super(name);
+        this.quantityOfPages = quantityOfPages;
+        this.year = year;
+        this.author = author;
+        this.pages = pages;
+    }
+
     int quantityOfPages;
     int year;
     String author;
-    String[]pages = new String[quantityOfPages + 1];
+    String[] pages = new String[quantityOfPages + 1];
 
     //n-ый элемент — текст на n-ой странице
 
-    pages [0] = super.name //0 элемент — строка вида {название книги, автор} (например, "Капитанская дочка, Пушкин")
 
+    //0 элемент — строка вида {название книги, автор} (например, "Капитанская дочка, Пушкин")
+
+
+    public String getPage(int n){
+        return pages[n];
+    }
 
 }
