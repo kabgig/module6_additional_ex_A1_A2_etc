fb4f7c9b6d39f80bf271231a669968f16ea65586
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-05 09:14:00 +0400

    ex2

---
+++ src/Knight.java
@@ -0,0 +1,9 @@
+public class Knight extends Soldier{
+    int price = 245;
+    String phrase = "Наше дело правое, а врага — найдем";
+
+    public Knight(String name) {
+        super(name);
+    }
+    public void feed(){}
+}
---
+++ src/Rifleman.java
@@ -0,0 +1,12 @@
+public class Rifleman extends Soldier{
+
+    int price = 205;
+    String phrase = "Мушкет и жену не дам никому";
+
+    public Rifleman(String name) {
+        super(name);
+    }
+
+    @Override
+    void attack(){}
+}
